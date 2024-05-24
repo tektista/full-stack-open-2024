eca76bbcaccf7d4b7de1155758e272d2d3569054
John Bautista <jnl.bautista.cs@gmail.com>
2024-05-24 15:11:31 +0100

    finish exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <div><Course course={course} /></div>;
+};
+
+export default App;
