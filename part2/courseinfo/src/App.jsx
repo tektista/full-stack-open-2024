8b92c206a435b919c25f4ea9ae0301978f8c5474
John Bautista <jnl.bautista.cs@gmail.com>
2024-05-24 18:10:16 +0100

    finish exercise 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,13 @@
+const Courses = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course, index) => (
+        <Course key={index} course={course} />
+      ))}
+    </>
+  );
+};
+
 const Course = ({ course }) => {
   return (
     <>
@@ -31,7 +41,6 @@ const Part = ({ part }) => {
 };
 
 const Total = ({ parts }) => {
-
   const total = parts.reduce(
     (sum, currentElement) => sum + currentElement.exercises,
     0
@@ -47,31 +56,54 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      <Courses courses={courses} />
     </div>
   );
 };
