09b09e7fc96a0caa29f5322787aa29f7782d12e5
John Bautista <jnl.bautista.cs@gmail.com>
2024-06-17 20:51:39 +0100

    finish exercise 3.9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,20 +1,21 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+//const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "http://localhost:3000/api";
 
 const getAll = () => {
-  return axios.get(baseUrl);
+  return axios.get(`${baseUrl}/persons`);
 };
 
 const create = (newObject) => {
-  return axios.post(baseUrl, newObject);
+  return axios.post(`${baseUrl}/persons`, newObject);
 };
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject);
+  return axios.put(`${baseUrl}/persons/${id}`, newObject);
 };
 
 const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
+  return axios.delete(`${baseUrl}/persons/${id}`);
 };
 
 export default {
